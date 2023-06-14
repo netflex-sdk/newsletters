a568c9c589c79e4092cd12c1e26b6716b06edf93
Thomas <thomas@alrek.no>
2023-06-14 10:15:43 +0000

    Improves loading time of StructureServiceProvider

--- NewsletterPage.php
+++ NewsletterPage.php
@@ -2,7 +2,6 @@
 
 namespace Netflex\Newsletters;
 
-use Illuminate\Support\Facades\App;
 use Netflex\Pages\AbstractPage;
 
 class NewsletterPage extends AbstractPage
@@ -22,16 +21,4 @@ class NewsletterPage extends AbstractPage
             ->where('type', static::TYPE_NEWSLETTER)
             ->values();
     }
-
-    public function getLangAttribute()
-    {
-      foreach($this->attributes['content'] as $content)
-      {
-        if($content['area'] === 'newsletterLocale') {
-          return $content['text'];
-        }
-      }
-      return $this->attributes['lang'] ?? App::getLocale();
-    }
-
 }
